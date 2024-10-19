895797d44ff8526ac0819bd69e35980fc91b2287
Ardit-K <arditkoti7192@gmail.com>
2024-10-19 14:00:38 -0400

    feat(game.js): Added playRound function.

--- game.js
+++ game.js
@@ -13,8 +13,41 @@ const getHumanChoice = () => {
     }
     return res;
 }
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    if (humanChoice === computerChoice) console.log("It's a Draw!");
+    else if (humanChoice === "rock" && computerChoice === "scissors"){
+        humanScore++;
+        console.log("You win! Rock beats Scissors!");
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper"){
+        computerScore++;
+        console.log("You lose! Paper beats Rock!");
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock"){
+        humanScore++;
+        console.log("You win! Paper beats Rock!");
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors"){
+        computerScore++;
+        console.log("You lose! Scissors beat Paper!");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock"){
+        computerScore++;
+        console.log("You lose! Rock beat Scissors!");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper"){
+        humanScore++;
+        console.log("You win! Scissors beat Paper!");
+    }
+}
 
 const compRes = getComputerChoice();
 const humanRes = getHumanChoice();
 console.log(`Computer Choice: ${compRes}`);
 console.log(`Human Choice: ${humanRes}`);
+playRound(humanRes, compRes);
