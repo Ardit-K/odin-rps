b2de9a9982bd4e99125d4221fd384d2de1602222
Ardit-K <arditkoti7192@gmail.com>
2024-10-19 13:41:23 -0400

    feat(game.js) Added random choice from computer.

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const getComputerChoice = () => {
+    let result = Math.round(Math.random() * 10) % 3;
+    if (result === 0) return "Rock";
+    else if(result === 1) return "Paper";
+    else return "Scissors";
+}
+
+const res = getComputerChoice();
+console.log(`Computer Choice: ${res}`);
