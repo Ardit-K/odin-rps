3d6f5e5d7af7030126b78198dd8dd42ff5b4de3c
Ardit-K <arditkoti7192@gmail.com>
2024-11-08 12:50:35 -0500

    feat(index.html/game.js) Added score and win logic

--- game.js
+++ game.js
@@ -18,36 +18,58 @@ const getHumanChoice = () => {
 let humanScore = 0;
 let computerScore = 0;
 
+const restart = () => {
+    humanScore = 0;
+    computerScore = 0;
+    document.querySelector('.pScore').textContent = 0;
+    document.querySelector('.cScore').textContent = 0;
+    document.querySelector('.result').textContent = "";
+    document.querySelector('.message').textContent = "";
+}
 const playRound = (humanChoice, computerChoice) => {
+    let winner = document.querySelector('.message');
     humanChoice = humanChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
     let result = document.querySelector('.result');
-    let pScore = document.querySelector('.pScore').textContent;
-    let cScore = document.querySelector('.cScore').textContent;
+    let pScore = document.querySelector('.pScore');
+    let cScore = document.querySelector('.cScore');
     if (humanChoice === computerChoice) console.log("It's a Draw!");
     else if (humanChoice === "rock" && computerChoice === "scissors"){
-        pScore++;
+        humanScore++;
+        pScore.textContent = humanScore;
         result.textContent = "You win! Rock beats Scissors!";
     }
     else if (humanChoice === "rock" && computerChoice === "paper"){
-        cScore++;
+        computerScore++;
+        cScore.textContent = computerScore;
         result.textContent = "You lose! Paper beats Rock!";
     }
     else if (humanChoice === "paper" && computerChoice === "rock"){
-        pScore++;
+        humanScore++;
+        pScore.textContent = humanScore;
         result.textContent = "You win! Paper beats Rock!";
     }
     else if (humanChoice === "paper" && computerChoice === "scissors"){
-        cScore++;
+        computerScore++;
+        cScore.textContent = computerScore;
         result.textContent = "You lose! Scissors beat Paper!";
     }
     else if (humanChoice === "scissors" && computerChoice === "rock"){
-        cScore++;
+        computerScore++;
+        cScore.textContent = computerScore;
         result.textContent = "You lose! Rock beat Scissors!";
     }
     else if (humanChoice === "scissors" && computerChoice === "paper"){
-        pScore++;
+        humanScore++;
+        pScore.textContent = humanScore;
         result.textContent = "You win! Scissors beat Paper!";
+    }
+
+    if(humanScore === 5){
+        winner.textContent = "You win!";
+    }
+    else if(computerScore === 5){
+        winner.textContent = "Computer Wins!";
     }
 }
 
